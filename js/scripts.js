62a9582960667b1544ee9258de41eeee90e2c71b
DronDrin <87070707+DronDrin@users.noreply.github.com>
2024-03-15 12:04:51 +0300

    Make video lazy

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
-let scrollY = 0;
-let animatingScroll = false;
-let firstBlockHeight;
+let loaded = false;
+
 $(window).on('load', () => {
     $(window).on('scroll', checkScroll);
     $(window).on('resize', checkScroll);
@@ -14,40 +13,25 @@ $(window).on('load', () => {
 
     $('.video__container').on('click', e => {
         $('.video').toggleClass('video_playing');
-        if ($('.video').hasClass('video_playing')) {
+        if ($('.video').hasClass('video_playing'))
             $('.video__video').get(0).play();
-        } else {
-            $('.video__video').get(0).pause();
-        }
+        else $('.video__video').get(0).pause();
     });
 
 
 });
 
 function checkScroll() {
-    if ($(window).scrollTop() > 200) {
+    if ($(window).scrollTop() > 200 && !loaded) {
         loadMap();
+        loadVideo();
+        loaded = true;
     }
-    if (animatingScroll) {
-        scrollY = $(window).scrollTop();
-        return;
-    }
-    firstBlockHeight = $(window).innerHeight() - 123;
-    let scrollingUp = scrollY - $(window).scrollTop() > 0;
-    if ($(window).scrollTop() > 20
-        && $(window).scrollTop() < firstBlockHeight - 20) {
-        animatingScroll = true;
-        $('html,body').animate({
-            scrollTop: scrollingUp ? 0 : firstBlockHeight
-        }, 'slow', '', () => {
-            setTimeout(() => animatingScroll = false, 10);
-        });
-        if (scrollingUp) {
-            setTimeout(() => $(document.body).removeClass('scrolled'), 70);
-        }
-    }
-    scrollY = $(window).scrollTop();
     $(document.body).toggleClass('scrolled', $(window).scrollTop() > 0);
+}
+
+function loadVideo() {
+    $('#lazy-video').attr('src', $('#lazy-video').attr('data-src'));
 }
 
 function loadMap() {
